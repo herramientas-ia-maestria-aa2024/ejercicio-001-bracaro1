3856ed962c653cd32907e1d95dc67f40cd5346d0
bracaro1 <166574294+bracaro1@users.noreply.github.com>
2024-04-10 17:53:30 -0500

    Update cod1.py

--- cod1.py
+++ cod1.py
@@ -1,13 +1,6 @@
-arc=open('/home/vboxuser/Descargas/informacion.txt')
-li=arc.readlines()
-for linea in li:
-  print (linea)
-
-file=open('C:/Users/byron.acaro/Downloads/informacion.txt')
+file=open('/home/vboxuser/Descargas/informacion.txt')
 line=file.readline()
-name_colum=line.split(';')
-print(name_colum[0],name_colum[1],name_colum[2],name_colum[3],sep='\t')
 while (line!=''):
     datos=line.split(';')
-    print (datos[0],datos[1],datos[2],datos[3],sep='\t')
+    print (datos[0],'\t\t',datos[1],'\t',datos[2],'\t',datos[3],end='\n')
     line=file.readline()
